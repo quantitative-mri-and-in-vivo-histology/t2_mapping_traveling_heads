725e98cbbd3035dd1d28dc0984d259711b0644ae
Laurin Mordhorst <laurin.mordhorst@gmx.de>
2024-10-16 20:14:22 +0200

    progress

--- scripts/prepare_kings_dataset.py
+++ scripts/prepare_kings_dataset.py
@@ -162,7 +162,7 @@ def main():
                                        b1_siemens_map_file=b1_map_file,
                                        b1_siemens_json_dict=b1_map_json_dict,
                                        b1_anat_ref_file=b1_anat_ref_file,
-                                       b1_anat_json_dict=b1_anat_json_dict,
+                                       b1_anat_ref_json_dict=b1_anat_ref_json_dict,
                                        b1_normalization_factor=b1_normalization_factor))
 
     print(inputs)
